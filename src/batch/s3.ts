6ccb0b36a62686fe8f03d30d73004dd47892a805
troyready <troy@troyready.com>
2019-12-26 13:02:37 -0800

    typedoc update

--- src/batch/s3.ts
+++ src/batch/s3.ts
@@ -1,17 +1,20 @@
 /**
- * /objects/batch S3 module
+ * This doesn't really need to exist separately from the primary API module,
+ * but it's currently easier to test as a separate module.
+ *
+ * Specifically, jest mocks of getSignedUrl need to be made from a
+ * client imported by "import * as S3 from 'aws-sdk/clients/s3'", while
+ * other mocks like headObject need to be made from a client imported by
+ * " import { S3 } from 'aws-sdk'".
  */
-
-// This doesn't really need to exist separately from the primary API module,
-// but it's currently easier to test as a separate module.
-// Specifically, jest mocks of getSignedUrl need to be made from a
-// client imported by "import * as S3 from 'aws-sdk/clients/s3'", while
-// other mocks like headObject need to be made from a client imported by
-// " import { S3 } from 'aws-sdk'".
 
 /** imports (delete comment after https://github.com/TypeStrong/typedoc/issues/603 resolution) */
 import { S3 } from "aws-sdk";
 
+/** S3 adapter class
+ *
+ * Each method maps to the corresponding S3 client method
+*/
 export class S3Adapter {
   public s3Client: S3;
   constructor() {
