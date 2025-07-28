da50a85b0a84a29bc9fceb1166ccbfcca57dd49e
radengoots <radengoots@users.noreply.github.com>
2025-07-28 12:05:33 +0200

    replace deprecated AWS Lambda types with updated one

--- src/authorizer/authorizer.ts
+++ src/authorizer/authorizer.ts
@@ -6,10 +6,11 @@
 
 import {
   Context,
-  CustomAuthorizerEvent,
-  CustomAuthorizerHandler,
-  CustomAuthorizerResult,
+  APIGatewayRequestAuthorizerEvent,
+  APIGatewayAuthorizerResult,
   PolicyDocument,
+  Handler,
+  AppSyncAuthorizerResult,
 } from "aws-lambda";
 import {
   AdminInitiateAuthCommand,
@@ -28,7 +29,7 @@ const cognitoIdpClient = new CognitoIdentityProviderClient({});
 
 /** Adapt the event methodArn to an IAM policy */
 function generateInvokePolicy(
-  event: CustomAuthorizerEvent,
+  event: APIGatewayRequestAuthorizerEvent,
   principal: string,
 ): { policyDocument: PolicyDocument; principalId: string } {
   const methodArnSections = event.methodArn.split(":");
@@ -107,10 +108,10 @@ export function getCredsFromAuthHeader(authHeader: string): {
 }
 
 /** AWS Lambda entrypoint */
-export const handler: CustomAuthorizerHandler = async (
-  event: CustomAuthorizerEvent,
+export const handler: Handler<APIGatewayRequestAuthorizerEvent, APIGatewayAuthorizerResult> = async (
+  event,
   context: Context, // eslint-disable-line @typescript-eslint/no-unused-vars
-): Promise<CustomAuthorizerResult> => {
+): Promise<APIGatewayAuthorizerResult> => {
   if (!event.headers) {
     throw new Error("No headers provided in event");
   }
